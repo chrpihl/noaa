decc85b7f807d79aa1782139cea19c59f7ac66a9
Christian Pihl Nielsen <cpn@trustpilot.com>
2018-03-20 10:48:23 +0100

    Added remaining unit tests

--- tests/testthat/test-mapping_tools.R
+++ tests/testthat/test-mapping_tools.R
@@ -13,7 +13,7 @@ test_that("df is missing annotation column", {
 test_that("ok mapping scenario", {
   df <- readr::read_delim(file = system.file("extdata", "signif.txt", package="noaa"), delim = "\t")
   map <- df %>% eq_clean_data() %>% eq_location_clean() %>% eq_map("DATE")
-  expect_true(any(class(m) == "htmlwidget"))
+  expect_true(any(class(map) == "htmlwidget"))
 })
 
 test_that("df is missing required column for label", {
@@ -26,9 +26,5 @@ test_that("ok create label scenario", {
   df <- readr::read_delim(file = system.file("extdata", "signif.txt", package="noaa"), delim = "\t")
   popup_text <- df %>% eq_create_label()
   expect_type(popup_text, "character")
-  print(nrow(df$LOCATION_NAME))
-  print(class(popup_text))
-  print(popup_text)
-  print(length(popup_text))
-  expect_equal(length(popup_text), nrow(df$LOCATION_NAME))
+  expect_equal(length(popup_text), nrow(df))
 })
--- tests/testthat/test-visualization_tools.R
+++ tests/testthat/test-visualization_tools.R
@@ -1,12 +1,73 @@
 context("test-visualization_tools.R")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("ok theme is returned", {
+  theme <- theme_time()
+  expect_true(any(class(theme) == "theme"))
+})
+
+test_that("geom_timeline empty df input is ok", {
+p <- readr::read_delim(file = system.file("extdata", "signif.txt", package="noaa"), delim="\t") %>%
+  eq_clean_data() %>% eq_location_clean() %>%
+  dplyr::filter(YEAR > 2100) %>%
+  ggplot2::ggplot() +
+  geom_timeline(ggplot2::aes(x = DATE, colour = DEATHS, size = EQ_MAG_ML))
+  expect_true(any(class(p) == "ggplot"))
+})
+
+test_that("geom_timeline missing required aesthetic fails", {
+  p <- readr::read_delim(file = system.file("extdata", "signif.txt", package="noaa"), delim="\t") %>%
+    eq_clean_data() %>% eq_location_clean() %>%
+    dplyr::filter(YEAR >= 1900, !is.na(DEATHS), !is.na(EQ_MAG_ML),
+                  COUNTRY %in% c("CHINA", "USA")) %>%
+    ggplot2::ggplot() +
+    geom_timeline(ggplot2::aes())
+  expect_that(ggplot2::ggplot_gtable(ggplot2::ggplot_build(p)), throws_error())
+})
+
+test_that("geom_timeline ok scenario", {
+  p <- readr::read_delim(file = system.file("extdata", "signif.txt", package="noaa"), delim="\t") %>%
+    eq_clean_data() %>% eq_location_clean() %>%
+    dplyr::filter(YEAR >= 1900, !is.na(DEATHS), !is.na(EQ_MAG_ML),
+                  COUNTRY %in% c("CHINA", "USA")) %>%
+    ggplot2::ggplot() +
+    geom_timeline(ggplot2::aes(x = DATE, colour = DEATHS, size = EQ_MAG_ML))
+  expect_true(any(class(p) == "ggplot"))
 })
 
 
-#geom_timeline
-#GeomTimeline
-#theme_time
+
 #geom_timeline_label
 #GeomTimelineLabel
+
+
+test_that("geom_timeline_label empty df input is ok", {
+  p <- readr::read_delim(file = system.file("extdata", "signif.txt", package="noaa"), delim="\t") %>%
+    eq_clean_data() %>% eq_location_clean() %>%
+    dplyr::filter(YEAR > 2100) %>%
+    ggplot2::ggplot() +
+    geom_timeline(ggplot2::aes(x = DATE, colour = DEATHS, size = EQ_MAG_ML)) +
+    geom_timeline_label(ggplot2::aes(label = LOCATION_NAME, n_max = 5))
+  expect_true(any(class(p) == "ggplot"))
+})
+
+test_that("geom_timeline_label missing required aesthetic fails", {
+ p <- readr::read_delim(file = system.file("extdata", "signif.txt", package="noaa"), delim="\t") %>%
+   eq_clean_data() %>% eq_location_clean() %>%
+   dplyr::filter(YEAR >= 1900, !is.na(DEATHS), !is.na(EQ_MAG_ML),
+                 COUNTRY %in% c("CHINA", "USA")) %>%
+   ggplot2::ggplot() +
+   geom_timeline(ggplot2::aes(x = DATE, colour = DEATHS, size = EQ_MAG_ML)) +
+   geom_timeline_label()
+ expect_that(ggplot2::ggplot_gtable(ggplot2::ggplot_build(p)), throws_error())
+})
+
+test_that("geom_timeline_label ok scenario", {
+ p <- readr::read_delim(file = system.file("extdata", "signif.txt", package="noaa"), delim="\t") %>%
+   eq_clean_data() %>% eq_location_clean() %>%
+   dplyr::filter(YEAR >= 1900, !is.na(DEATHS), !is.na(EQ_MAG_ML),
+                 COUNTRY %in% c("CHINA", "USA")) %>%
+   ggplot2::ggplot() +
+   geom_timeline(ggplot2::aes(x = DATE, colour = DEATHS, size = EQ_MAG_ML)) +
+   geom_timeline_label(ggplot2::aes(label = LOCATION_NAME, n_max = 5))
+ expect_true(any(class(p) == "ggplot"))
+})
